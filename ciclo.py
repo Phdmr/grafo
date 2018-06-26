b923ab241520ee9a4702328123dc7d01a6dff085
Phdmr <33499845+Phdmr@users.noreply.github.com>
2018-06-26 02:16:18 -0300

    Create ciclo.py

---
+++ ciclo.py
@@ -0,0 +1,89 @@
+grafo = {0: [2, 3],
+         1: [4, 8],
+         2: [5],
+         3: [7, 8],
+         4: [8],
+         5: [6],
+         6: [2],
+         7: [9],
+         8: [0],
+         9: [7]}
+
+def edge_list(G):
+    links = []
+    for u in G:
+        for v in G[u]:
+            links.append((u, v))
+    return links
+
+
+def achar_aresta(gr):
+    list_ares = []
+    for key in gr:
+        list_ares.append(key)
+    return list_ares
+
+
+def create(a, b):
+    return (a, b)
+
+
+def achar_ciclo(graph, start):
+    vertexList, edgeList = graph
+    print(graph)
+    visitedVertex = []
+    stack = [start]
+    adjacencyList = [[] for v in vertexList]
+    print(adjacencyList)
+
+    for edge in edgeList:
+        print("edge: ", edge)
+        adjacencyList[edge[0]].append(edge[1])
+        print(adjacencyList)
+
+    while stack:
+        print('Pilha ', stack)
+        current = stack.pop()
+        print("estamos em: ", current)
+        for neighbor in adjacencyList[current]:
+            print("oi vizinho: ", neighbor)
+            if not neighbor in visitedVertex:
+                stack.append(neighbor)
+                y = None
+                print(y)
+            else:
+                y = neighbor
+                print("ja estive aqui: ", y)
+        if y is None:
+            visitedVertex.append(current)
+            print("foram visitados: ", visitedVertex)
+        else:
+            print("Cilco achado")
+            visitedVertex.append(current)
+            visitedVertex.append(y)
+            g = []
+            for i in reversed(visitedVertex):
+                if i not in g:
+                    g.append(i)
+                else:
+                    g.append(i)
+                    break
+            g = g[::-1]
+            return g
+
+
+def bfs(graph, start):
+    vertexList, edgeList = graph
+    visitedList = []
+    queue = [start]
+    adjacencyList = [[] for vertex in vertexList]
+    for edge in edgeList:
+        adjacencyList[edge[0]].append(edge[1])
+        print(edge)
+        print(adjacencyList)
+
+edge = (edge_list(grafo))
+arestas = (achar_aresta(grafo))
+grafoo = (create(arestas, edge))
+
+print(achar_ciclo(grafoo, 1))
